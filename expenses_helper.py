4fcb2681f0b36594fb32f38aa8afca9138175958
David8882 <dgolego@gmail.com>
2024-11-01 13:52:54 +0200

    Home service

--- expenses_helper.py
+++ expenses_helper.py
@@ -7,3 +7,33 @@ from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
+
+
+class DatabaseManager:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+
+    def create_tables(self):
+        # Create tables if they don't exist
+        pass
+
+    def insert_data(self, data):
+        # Code to insert data
+        pass
+
+    def update_data(self, data):
+        # Code to update data
+        pass
+
+    def delete_data(self, id):
+        # Code to delete data
+        pass
+
+    def fetch_data(self):
+        # Code to fetch all data
+        pass
+
+    def fetch_resit_data(self):
+        # Code to fetch all resit data
+        pass
