f4e668402ef157d55394e8715c8e07bdd3738aa6
David8882 <dgolego@gmail.com>
2024-11-02 18:31:53 +0200

    Home service

--- backend.py
+++ backend.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    jsonify({'message': 'check backend'})
     return jsonify({'message': 'ברוכים הבאים למיקרוסרוויס של ה-Back!'})
 
 if __name__ == '__main__':
